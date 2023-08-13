4697af128a9ea5801d9ad91a75506a11ceeb3ab3
Amir H. Yeganemehr <yeganemehr@yahoo.com>
2023-08-13 20:01:40 +0330

    Remove useless code

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,184 +1,34 @@
 <template>
   <v-container class="toolbar-container">
     <v-toolbar density="compact" color="white">
-      <v-row>
-        <v-col sm="2" cols="6">
-          <div class="logo-size">
-            <router-link :to="{ name: 'home' }">
-              <v-img
-              src="@/assets/pics/JeyDomain.svg"
-              class="mt-4 mt-sm-2">
-              </v-img>
-            </router-link>
-          </div>
-        </v-col>
-        <v-col sm="7" cols="1">
-          <div class="visible">
-            <v-row>
-              <v-col cols="2">
-                <v-btn class="toolbar-btn" :to="{ name: 'home' }">
-                  صفحه ی اصلی
-                </v-btn>
-              </v-col>
-              <v-col cols="2">
-                <v-btn class="toolbar-btn" href="#"> تعرفه ها </v-btn>
-              </v-col>
-              <v-col cols="2">
-                <v-btn class="toolbar-btn" href="#"> سوالات متداول </v-btn>
-              </v-col>
-              <v-col cols="2">
-                <v-btn class="toolbar-btn" :to="{ name: 'contact-us' }">
-                  تماس با ما
-                </v-btn>
-              </v-col>
-              <v-col cols="2">
-                <v-btn class="toolbar-btn" href="#"> بلاگ </v-btn>
-              </v-col>
-            </v-row>
-          </div>
-        </v-col>
-        <v-col sm="3" cols="5">
-          <v-app-bar-nav-icon
-            class="float-left nav-icon-visible"
-            @click="$emit('clickOnNavBtn')">
-          </v-app-bar-nav-icon>
-          <div class="float-left visible">
-            <v-btn class="toolbar-btn" href="#"> ورود </v-btn>
-            <v-btn
-              class="toolbar-btn"
-              color="primary"
-              rounded="xl"
-              variant="flat"
-              href="#">
-              ثبت نام
-            </v-btn>
-            <v-btn color="secondary" class="toolbar-btn hidden-sm-and-down" :to="{ name: 'blog' }"> بلاگ </v-btn>
-            <v-app-bar-nav-icon class="hidden-md-and-up" @click="$emit('clickOnNavBtn')"> </v-app-bar-nav-icon>
-          </div>
-        </v-col>
-      </v-row>
+      <v-toolbar-title>
+        <router-link :to="{ name: 'home' }">
+          <v-img src="@/assets/pics/JeyDomain.svg" max-width="150" />
+        </router-link>
+      </v-toolbar-title>
+      <v-toolbar-items class="hidden-sm-and-down">
+        <v-btn :to="{ name: 'prices' }"> تعرفه ها </v-btn>
+        <v-btn :to="{ name: 'questions' }"> سوالات متداول </v-btn>
+        <v-btn :to="{ name: 'contact-us' }">تماس با ما</v-btn>
+        <v-btn :to="{ name: 'blog' }">بلاگ</v-btn>
+      </v-toolbar-items>
+      <v-spacer />
+
+      <v-toolbar-items class="hidden-sm-and-down">
+        <v-btn class="toolbar-btn" :to="{ name: 'logIn' }"> ورود </v-btn>
+        <v-btn class="toolbar-btn" color="primary" rounded="xl" variant="flat" :to="{ name: 'register' }">
+          ثبت نام
+        </v-btn>
+      </v-toolbar-items>
+      <v-app-bar-nav-icon class="hidden-md-and-up" @click="$emit('clickOnNavBtn')"> </v-app-bar-nav-icon>
     </v-toolbar>
   </v-container>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import DefaultUserImage from "@/assets/pics/user.jpg";
-import JeyDomain from "@/assets/pics/JeyDomain.svg";
-import { useUserState } from "@/stores/UserState";
-import { mapState } from "pinia";
-import { persianNumber } from "@/utilities";
-import { useCartStore } from "@/stores/Cart";
 
 export default defineComponent({
-    emits: ["clickOnNavBtn"],
-    setup(){
-        return{
-            persianNumber,
-            cartStore: useCartStore(),
-            userStore: useUserState()
-        }
-    },
-    data() {
-        return {
-            JeyDomain: JeyDomain,
-            items: [
-                { title: 'پنل کاربری', href: '/user-panel/dashboard' },
-                { title: 'مشاهده پروفایل', href: '' },
-                { title: 'خروج از حساب', href: '' },
-            ],
-            user: {
-                avatar: DefaultUserImage,
-            },
-            numberOfNotifications: 3,
-        };
-    },
-    computed: {
-        ...mapState(useUserState, { userState: "userState" })
-    },
+  emits: ["clickOnNavBtn"],
 });
 </script>
-
-<style lang="scss">
-#nav-container {
-  padding-bottom: 0px;
-  padding-top: 0px;
-}
-.nav-dropdown-menu {
-  .v-list-item-title {
-    font-size: 12px;
-  }
-}
-.navbar-container { 
-  .nav-btn {
-    --v-theme-overlay-multiplier: 0;
-    font-size: 12.5px;
-    margin-right: -10px;
-    --v-theme-overlay-multiplier: 0;
-  }
-  .circle {
-    width: 20px;
-    height: 20px;
-    padding: 1.5px;
-    font-size: 10px;
-    text-align: center;
-    color: white !important;
-    margin: -33px -10px 0px 0px;
-  }
-  .search-form {
-    width: 200px;
-    height: 30px;
-    padding: 5px;
-    margin: 10px 30px;
-    .v-fiels {
-      height: 10px;
-    }
-    .v-field--variant-plain.v-field {
-      --v-field-padding-top: 0px;
-      --v-field-input-padding-top: 0px;
-      font-size: 12px;
-    }
-    .v-input--density-compact {
-      --v-input-padding-top: 8px;
-      --v-field-padding-top: 0px;
-    }
-    .v-field.v-field--variant-plain {
-      .v-field__prepend-inner {
-        padding-top: 0px;
-      }
-    }
-  }
-  .logo-size {
-    width: 170px;
-  }
-  .login-register-btn {
-    font-size: 13px;
-    font-weight: 300;
-    --v-theme-overlay-multiplier: 0;
-  }
-  .toolbar-btn {
-    font-size: 13px;
-    margin: auto 15px;
-    font-weight: 600;
-    --v-theme-overlay-multiplier: 0;
-  }
-  .nav-btn-md {
-    display: block;
-  }
-  .nav-icon-xs-sm {
-    display: none;
-  }
-  @media (max-width: 960px) {
-    .nav-btn-md {
-      display: none;
-    }
-    .nav-icon-xs-sm {
-      display: block;
-    }
-  }
-  --v-theme-overlay-multiplier: 0;
-  .overlay-btn{
-    --v-theme-overlay-multiplier: 0;
-  }
-}
-</style>
