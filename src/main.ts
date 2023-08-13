6f6d89fb272cfcae921ab77970873a2817ef440b
Amir H. Yeganemehr <yeganemehr@yahoo.com>
2023-08-13 19:23:57 +0330

    move pinia from main to plugins

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,9 @@
-/**
- * main.ts
- *
- * Bootstraps Vuetify and other plugins then mounts the App`
- */
-
-// Components
 import App from './App.vue';
-
-// Composables
 import { createApp } from 'vue';
-import { createPinia } from 'pinia';
-
-// Plugins
 import { registerPlugins } from '@/plugins';
 
-const pinia = createPinia();
 const app = createApp(App);
 
 registerPlugins(app);
 
-app.use(pinia);
 app.mount('#app');
-
--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -1,7 +1,12 @@
 import vuetify from './vuetify';
 import router from '../router';
 import type { App } from 'vue';
+import { createPinia } from 'pinia';
+
 
 export function registerPlugins (app: App) {
-    app.use(vuetify).use(router);
+    const pinia = createPinia();
+    app.use(vuetify);
+    app.use(router);
+    app.use(pinia);
 }
